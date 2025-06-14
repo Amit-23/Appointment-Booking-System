bf8d95fe65298ecb49723ae752eafe14358f98cd
Amit-23 <amitsanghalsingh@gmail.com>
2025-06-14 11:33:32 +0530

    added model for appointments

--- backend/appointmentHub_backend/authentication/models.py
+++ backend/appointmentHub_backend/authentication/models.py
@@ -24,3 +24,43 @@ class UserAccount(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.role})"
+
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'), # Appointment booked but not responded to
+        ('accepted', 'Accepted'), # Freelancer has accepted it
+        ('rejected', 'Rejected'), # Freelancer has rejected it
+        ('cancelled', 'Cancelled'), # Client cancelled the appointment
+        ('completed', 'Completed'), # Appointment has been completed
+    ]
+
+    # client: who booked the appointment
+    client = models.ForeignKey(
+        'UserAccount',
+        on_delete=models.CASCADE,
+        related_name='appointments_made'  # access with client.appointments_made.all()
+    )
+
+    # freelancer: who was booked
+    freelancer = models.ForeignKey(
+        'UserAccount',
+        on_delete=models.CASCADE,
+        related_name='appointments_received'  # access with freelancer.appointments_received.all()
+    )
+
+    # Date and time of the appointment
+    date = models.DateField()
+    
+
+    # Status of the appointment
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    # Optional message or reason from client while booking
+    message = models.TextField(blank=True, null=True)
+
+    # Auto-set when the appointment was created
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.client.name} → {self.freelancer.name} on {self.date} ({self.status})"
