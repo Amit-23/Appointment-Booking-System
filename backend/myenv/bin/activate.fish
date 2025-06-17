c19d3792b98038dcbe7909b988231692e4ba2708
Amit-23 <amitsanghalsingh@gmail.com>
2025-06-17 06:48:19 +0000

    added requirements.txt

--- backend/myenv/bin/activate.fish
+++ backend/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/amit/Desktop/AppointmentHub/backend/myenv
+set -gx VIRTUAL_ENV /home/vboxuser/Desktop/Appointment-Booking-System/backend/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
